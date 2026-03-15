e70f31ecb89550c2ade3cc2a1770e1ee0461e91b
talatbulut <130913942+talatbulut3416@users.noreply.github.com>
2026-03-16 00:23:42 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:23:26.290Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-15T21:23:42.986Z
+Commit: Fix race condition in async operations
