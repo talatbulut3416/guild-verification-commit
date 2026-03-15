882d36ee099541c672990925863240b410f299c1
talatbulut <130913942+talatbulut3416@users.noreply.github.com>
2026-03-16 00:24:27 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:24:12.975Z
-Commit: Add dark mode support
+Timestamp: 2026-03-15T21:24:27.813Z
+Commit: Patch security vulnerability in authentication
