6d38ce85237dddbd2512e4b23817ab6250be5911
talatbulut <130913942+talatbulut3416@users.noreply.github.com>
2026-03-16 00:24:25 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:24:20.479Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-15T21:24:25.807Z
+Commit: Fix memory leak in event handlers
