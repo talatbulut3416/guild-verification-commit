5c9646f03db4b155991c2fd05a22c339e74ec4ad
talatbulut <130913942+talatbulut3416@users.noreply.github.com>
2026-03-16 00:24:38 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:24:28.711Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-15T21:24:38.695Z
+Commit: Correct typo in configuration file
