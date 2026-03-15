3f9ffec4cd92b2a8470749fb7f132eb33ccec065
talatbulut <130913942+talatbulut3416@users.noreply.github.com>
2026-03-16 00:24:24 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:24:11.924Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-15T21:24:24.722Z
+Commit: Fix memory leak in event handlers
