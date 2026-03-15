89dce8b7069c716fd3e611998c389af28cc5ec8c
talatbulut <130913942+talatbulut3416@users.noreply.github.com>
2026-03-16 00:23:18 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:23:08.327Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-15T21:23:18.508Z
+Commit: Fix broken links in documentation
