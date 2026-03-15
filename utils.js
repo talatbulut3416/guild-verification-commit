1ca20197d982e9721837f466e06ae2b9b5e67e66
talatbulut <130913942+talatbulut3416@users.noreply.github.com>
2026-03-16 00:23:54 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:23:42.986Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-15T21:23:54.476Z
+Commit: Fix CSS styling bug on mobile devices
