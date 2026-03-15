7c1e1f99c21418485366f0082cbde985568a0905
talatbulut <130913942+talatbulut3416@users.noreply.github.com>
2026-03-16 00:23:58 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:23:19.726Z
-Commit: Add multi-language support
+Timestamp: 2026-03-15T21:23:58.507Z
+Commit: Resolve timezone issues in date handling
